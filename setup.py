046fc4c4e6c7f30dcbe37423d850132959bc9018
montag451 <montag451@laposte.net>
2018-12-21 13:01:55 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='python-pypi-mirror',
-    version='1.0.0',
+    version='1.0.1',
     author='montag451',
     author_email='montag451@laposte.net',
     maintainer='montag451',
@@ -14,7 +14,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
     ],
-    packages=find_packages(),
+    py_modules=['pypi_mirror'],
     entry_points={
         'console_scripts': [
             'pypi_mirror=pypi_mirror:main'
