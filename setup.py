822417a0148a41bfa45ca477b1bbdc1e2d825867
montag451 <montag451@laposte.net>
2022-05-16 01:04:46 +0200

    Bump version number (5.0.0 -> 5.0.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="python-pypi-mirror",
-    version="5.0.0",
+    version="5.0.1",
     author="montag451",
     author_email="montag451@laposte.net",
     maintainer="montag451",
