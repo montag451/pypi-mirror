ce5b3ea8fce5faf576182aa5641c3274412b04a5
montag451 <montag451@laposte.net>
2022-05-15 23:09:50 +0200

    Require Python 3.7 and above

--- setup.py
+++ setup.py
@@ -10,10 +10,15 @@ setup(
     url='https://github.com/montag451/pypi-mirror',
     description='A script to create a partial PyPI mirror',
     long_description=open('README.rst').read(),
+    python_requires=">=3.7",
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     py_modules=['pypi_mirror'],
     entry_points={
