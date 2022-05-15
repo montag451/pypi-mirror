97b3eceaef188e0b402c41faaf1cd7abc702a803
montag451 <montag451@laposte.net>
2022-05-15 23:10:48 +0200

    Blacken setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 from setuptools import setup
 
 setup(
-    name='python-pypi-mirror',
-    version='4.2.0',
-    author='montag451',
-    author_email='montag451@laposte.net',
-    maintainer='montag451',
-    maintainer_email='montag451@laposte.net',
-    url='https://github.com/montag451/pypi-mirror',
-    description='A script to create a partial PyPI mirror',
-    long_description=open('README.rst').read(),
+    name="python-pypi-mirror",
+    version="4.2.0",
+    author="montag451",
+    author_email="montag451@laposte.net",
+    maintainer="montag451",
+    maintainer_email="montag451@laposte.net",
+    url="https://github.com/montag451/pypi-mirror",
+    description="A script to create a partial PyPI mirror",
+    long_description=open("README.rst").read(),
     python_requires=">=3.7",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    py_modules=['pypi_mirror'],
-    entry_points={
-        'console_scripts': [
-            'pypi-mirror=pypi_mirror:main'
-        ]
-    }
+    py_modules=["pypi_mirror"],
+    entry_points={"console_scripts": ["pypi-mirror=pypi_mirror:main"]},
 )
